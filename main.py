ae9ae5ff1355c12ac50d13318e24154f0d63ff28
Vlad <ionescupv@gmail.com>
2021-11-17 22:12:32 +0200

    this must be final

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from functools import wraps
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
